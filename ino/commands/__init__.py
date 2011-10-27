489051290768094c9a1de5f0467f91355d4d0739
Victor Nakoryakov <nail.xx@gmail.com>
2011-10-27 17:42:44 +0400

    + serial command

--- ino/commands/__init__.py
+++ ino/commands/__init__.py
@@ -4,3 +4,4 @@ from ino.commands.init import Init
 from ino.commands.build import Build
 from ino.commands.clean import Clean
 from ino.commands.upload import Upload
+from ino.commands.serial import Serial
---
+++ ino/commands/serial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8; -*-
+
+import subprocess
+
+from ino.commands.base import Command, CommandError
+
+
+class Serial(Command):
+
+    name = 'serial'
+
+    def setup_arg_parser(self, parser):
+        parser.add_argument('-p', '--serial-port', metavar='PORT', default='/dev/ttyACM0', 
+                            help='Serial port to communicate with')
+        parser.add_argument('-b', '--baud-rate', metavar='RATE', type=int, default=9600, 
+                            help='Communication baud rate, should match value set in Serial.begin() on Arduino')
+
+    def run(self, args):
+        serial_monitor = 'picocom'
+        subprocess.call([
+            serial_monitor,
+            args.serial_port,
+            '-b', str(args.baud_rate),
+            '-l',
+        ])
