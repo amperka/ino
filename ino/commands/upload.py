8439949f1db6b814cd41e446def18b03439d311d
Victor Nakoryakov <nail.xx@gmail.com>
2011-10-27 23:51:26 +0400

    + upload command board selection

--- ino/commands/upload.py
+++ ino/commands/upload.py
@@ -19,7 +19,33 @@ class Upload(Command):
 
     def setup_arg_parser(self, parser):
         parser.add_argument('-p', '--serial-port', metavar='PORT', default='/dev/ttyACM0', 
-                            help='Serial port to upload firmware to')
+                            help='Serial port to upload firmware to\n(default: %(default)s)')
+
+        try:
+            boards = self.e.board_models()
+        except Abort:
+            boards = {}
+
+        parser.add_argument('-m', '--board-model', metavar='MODEL', default='uno', choices=boards.keys(),
+                            help='Arduino board model. See below.')
+
+        parser.add_argument('-d', '--arduino-dist', metavar='PATH',
+                            help='Path to Arduino distribution, e.g. ~/Downloads/arduino-0022.\nTry to guess if not specified')
+
+    def epilog(self):
+        try:
+            boards = self.e.board_models()
+        except Abort:
+            return "Board description file (boards.txt) not found, so board model list is unavailable.\n" \
+                    "Use --arduino-dist option to specify its location."
+
+        import ino.filters
+        c = ino.filters.colorize
+        default_mark = c('[DEFAULT] ', 'red')
+        boards = ['%s: %s%s' % (c('%12s' % key, 'cyan'), default_mark if key == 'uno' else '', val['name']) 
+                  for key, val in boards.iteritems()]
+
+        return '\n'.join(['Supported Arduino board models:\n'] + boards)
 
     def discover(self):
         self.e.find_tool('stty', ['stty'])
@@ -29,6 +55,13 @@ class Upload(Command):
     def run(self, args):
         self.discover()
         port = args.serial_port
+        board = self.e.board_models()[args.board_model]
+
+        protocol = board['upload']['protocol']
+        if protocol == 'stk500':
+            # if v1 is not specifid explicitly avrdude will
+            # try v2 first and fail
+            protocol = 'stk500v1'
 
         if not os.path.exists(port):
             raise Abort("%s doesn't exist. Is Arduino connected?" % port)
@@ -50,10 +83,10 @@ class Upload(Command):
         subprocess.call([
             self.e['avrdude'],
             '-C', self.e['avrdude.conf'],
-            '-p', 'atmega328p',
+            '-p', board['build']['mcu'],
             '-P', port,
-            '-c', 'stk500v1',
-            '-b', '115200',
+            '-c', protocol,
+            '-b', board['upload']['speed'],
             '-D',
             '-U', 'flash:w:%s:i' % self.e['hex_path'],
         ])
